d8b83feff665866ca693278ba29446be6d925eaa
efscoob <genxxx@rambler.ru>
2017-10-01 21:41:08 +0400

    Add HostInfo in urlManager

--- frontend/config/urlManager.php
+++ frontend/config/urlManager.php
@@ -1,6 +1,11 @@
 <?php
+/**
+ * @var array $params
+ */
+
 return [
     'class' => 'yii\web\UrlManager',
+    'hostInfo' => $params['frontendHostInfo'],
     'enablePrettyUrl' => true,
     'showScriptName' => false,
     'rules' => [
